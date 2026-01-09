ee08d8dab0543f57aad7d39944ad16cd4d11bb52
Mike Sperber <sperber@deinprogramm.de>
2026-01-09 13:41:27 +0100

    Get to working nix develop for Liquid Haskell

--- flake.nix
+++ flake.nix
@@ -2,7 +2,8 @@
   description = "FM";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/release-25.05";
+    # nixpkgs.url = "github:nixos/nixpkgs/release-25.11";
+    nixpkgs.url = "path:/Users/sperber/build/nixpkgs";
     flake-parts.url = "github:hercules-ci/flake-parts";
   };
 
@@ -31,31 +32,12 @@
                 in
                 {
                   haskellPackages = prev.haskell.packages."ghc${ghcVersion}".override (old: {
-                    # https://niteria.github.io/posts/nix-notes-overriding-haskell-packages/
-                    all-cabal-hashes = pkgs.fetchurl {
-                      url = "https://github.com/commercialhaskell/all-cabal-hashes/archive/8ac76f327d5861499e4ebe357ec7159222bffdd9.tar.gz";
-                      sha256 = "039rz01rv7wll50xbllifkkw4vl087awh446zkif27kv1738nrc8";
-                    };
                     overrides = final.lib.composeExtensions (old.overrides or (_: _: { })) (
                       hfinal: hprev: {
-                        # tasty dependency version has unsupported version
-                        smtlib-backends-tests = hlib.dontCheck (hlib.unmarkBroken hprev.smtlib-backends-tests);
-                        smtlib-backends-process = hlib.dontCheck (hlib.unmarkBroken hprev.smtlib-backends-process);
-
-                        # 0.7.20, wants 0.7.18
                         store = hlib.dontCheck hprev.store;
-
-                        liquid-fixpoint = hlib.unmarkBroken hprev.liquid-fixpoint;
-                        cabal2nix = hlib.dontCheck hprev.cabal2nix;
-                        # newer, works with ghc 9.12
-                        ghc-tcplugins-extra = hprev.ghc-tcplugins-extra_0_5;
-                        # newer, works with ghc 9.12
-                        Cabal = hprev.Cabal_3_14_2_0;
-                        #  z3: createProcess: posix_spawnp: does not exist (No such file or directory)
-                        rest-rewrite = hlib.dontCheck (hfinal.callHackage "rest-rewrite" "0.4.5" {});
-                        # 0.9.12.2
-                        liquidhaskell = hlib.addBuildDepend pkgs.z3 hprev.liquidhaskell;
-                        liquid-vector = hlib.addBuildDepend pkgs.z3 hprev.liquid-vector;
+                        liquidhaskell-boot = hprev.liquidhaskell-boot_0_9_12_2;
+                        liquid-fixpoint = hlib.dontCheck hprev.liquid-fixpoint_0_9_6_3_3;
+                        liquidhaskell = hprev.liquidhaskell_0_9_12_2;
                       }
                     );
                   });
@@ -65,8 +47,11 @@
           };
           hlib = pkgs.haskell.lib.compose;
           liquid-haskell =
-            pkgs.haskellPackages.callCabal2nix "liquid-haskell" (pkgs.lib.cleanSource ./liquid-haskell)
-              { };
+            (pkgs.haskellPackages.callCabal2nix "liquid-haskell" (pkgs.lib.cleanSource ./liquid-haskell) { })
+            .overrideAttrs
+              (old: {
+                nativeBuildInputs = (old.nativeBuildInputs or [ ]) ++ [ pkgs.z3 ];
+              });
         in
         {
           formatter = pkgs.nixfmt;
@@ -109,7 +94,7 @@
           };
 
           legacyPackages = pkgs;
-          
+
           packages = {
             inherit liquid-haskell;
             inherit (pkgs) cabal-install;
@@ -122,7 +107,7 @@
             # in order to grab the `ghcWithPackages` from out of there. That way
             # we're able to globally install this GHC in the Docker image and
             # get rid of direnv as a dependency.
-            ghcForFunar = builtins.head self.devShells.${system}.default.nativeBuildInputs;
+            ghcForFm = builtins.head self.devShells.${system}.default.nativeBuildInputs;
 
             watch = pkgs.writeShellScriptBin "watch-and-commit" ''
               ${pkgs.lib.getExe pkgs.watch} -n 10 "git add . && git commit -m update && git push"
